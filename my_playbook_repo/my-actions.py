b8efbb95ba2a898b7b1c69a59e1523a9bdc6f49a
HamzaXgrid <hamza.arshad@xgrid.co>
2024-01-24 16:03:44 +0500

    Get pod Name and check for pvc v1.1.7

--- my_playbook_repo/my-actions.py
+++ my_playbook_repo/my-actions.py
@@ -38,7 +38,7 @@ def checkUnboundPv(event: PodEvent):
     for volume in pod.spec.volumes:
         print("volume is",volume)
         #if volume.persistent_volume_claim:
-        pvc_name = volume.persistentVolumeClaim.claimName
+        pvc_name = pod.spec.volumes.persistentVolumeClaim.claimName
         print("pvc is",pvc_name)
         print(f"PersistentVolumeClaim Name for Pod {podName}: {pvc_name}")
     finding.title = f"Pod Content:"
